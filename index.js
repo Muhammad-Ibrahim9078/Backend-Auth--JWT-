5bac50f8e1fde0a904a3a1a460e2383f79955f5b
M.Ibrahim <m.iibrahim9078@gmail.com>
2025-11-10 09:40:25 +0500

    backend Auth with jwt

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+import 'dotenv/config';
+import authRoutes from './src/routes/authRoutes.js';
+import userRoutes from './src/routes/userRoutes.js';
+import dbConnect from './src/config/db.js';
+import cookieParser from 'cookie-parser';
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+dbConnect();
+app.use('/', authRoutes);
+app.use('/', userRoutes);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
